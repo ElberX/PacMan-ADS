4fc15b5ee3a6fb3f00a4ac70fbfe788270009a06
Elber <elbermonteiro011@gmail.com>
2023-06-07 21:38:56 -0300

    README

--- README.py
+++ README.py
@@ -10,6 +10,27 @@ ELBER MONTEIRO (MATRÍCULA: 01570542)
 YRVINNY THUANY (MATRÍCULA: 01599519)
 MATHEUS EMANUEL (MATRÍCULA: 01590369)
 
+# Jogo Pacman
+
+Este é um jogo simples do Pacman implementado em Python. O jogador controla o Pacman e seu objetivo é
+comer todas as comidas representadas por "X" no grid antes de atingir uma pontuação de 10.
+
+## Funcionamento do Jogo
+O jogo começa com um menu de opções que permite iniciar o jogo ou sair. Ao selecionar a opção "INICIAR", o jogo é iniciado. O Pacman é
+representado por um "o" amarelo e também por 'O' vermelho e as comidas são representadas por "X" verde.
+O jogador controla o movimento do Pacman usando as teclas W, A, S e D para cima, esquerda, baixo e direita, respectivamente.
+
+Após cada movimento do Pacman, o grid é atualizado para refletir a nova posição do Pacman e a interação com as comidas.
+Se o Pacman alcançar uma comida, a pontuação é incrementada e a comida é reposicionada aleatoriamente no grid. Quando a pontuação atingir 10, o jogador vence o jogo.
+
+## Requisitos
+Este jogo requer Python 3.x para ser executado.
+
+## Executando o Jogo
+Para executar o jogo, execute o seguinte comando no terminal:
+
+python pacman.py
+
 
 
 
